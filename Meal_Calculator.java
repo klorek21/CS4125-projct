5bd12df0c206946b788cf9d9aa88c4bc0cb9a54d
Elton111 <58089190+Elton111@users.noreply.github.com>
2022-12-03 00:07:58 +0000

    Add files via upload

---
+++ Meal_Calculator.java
@@ -0,0 +1,263 @@
+//##########################################################################################################################################
+// Java program to demonstrate working of
+// Factory Design Pattern
+import Decoratorvb;
+
+// Interface named Meal
+interface Meal {
+
+	// Method inside interface
+	void createMeal();
+ }
+ 
+ 
+ // Class 1
+ // Class 1 will be implementing the Meat interface
+ // Meat.java
+ class Meat implements Meal {
+ 
+	// Overriding the method
+	@Override
+	public void createMeal() {
+	
+	   // Print statement to execute when
+	   // createMeal() method of this class is called
+	   // later on in the main() method
+	   System.out.println("Inside Meat::createMeal() method.");
+	}
+ }
+ 
+ 
+ // Class 2
+ // Abstract class
+ // Vegetables.java
+ class Vegetables implements Meal {
+ 
+	// Overriding the method
+	@Override
+	public void createMeal() {
+	
+	   // Display message
+	   System.out.println("Inside Vegetables::createMeal() method.");
+	}
+ }
+ 
+ 
+ // Class 3
+ // Concrete class extending the abstract class
+ // Carbs.java
+ class Carbs implements Meal {
+ 
+	// Overriding the method
+	@Override
+	public void createMeal() {
+	
+	   // Display message
+	   System.out.println("Inside Carbs::createMeal() method.");
+	}
+ }
+ 
+ 
+ // Class 4
+ // MealFactory.java
+ class MealFactory {
+	 
+	// Use getMeal method to get object of type meal 
+	public Meal getMeal(String mealType){
+	   if(mealType == null){
+		  return null;
+	   }		
+	   if(mealType.equalsIgnoreCase("CARBS")){
+		  return new Carbs();
+		  
+	   } else if(mealType.equalsIgnoreCase("MEAT")){
+		  return new Meat();
+		  
+	   } else if(mealType.equalsIgnoreCase("VEGETABLES")){
+		  return new Vegetables();
+	   }
+	   
+	   return null;
+	}
+ }
+
+//##########################################################################################################################################
+// State Design Pattern
+interface Subscriptions
+{
+	public void user(SubscriptionState ctx);
+}
+
+class Bronze implements Subscriptions
+{
+	@Override
+	public void user(SubscriptionState ctx)
+	{
+		System.out.println("Bronze Member");
+	}
+
+}
+
+class Silver implements Subscriptions
+{
+	@Override
+	public void user(SubscriptionState ctx)
+	{
+		System.out.println("Silver Memeber");
+	}
+
+}
+
+class Gold implements Subscriptions
+{
+	@Override
+	public void user(SubscriptionState ctx)
+	{
+		System.out.println("Gold Memeber");
+	}
+
+}
+
+class SubscriptionState
+{
+	private Subscriptions userState;
+
+	public SubscriptionState()
+	{
+		userState = new Bronze();
+	}
+
+	public void setState(Subscriptions state)
+	{
+		userState = state;
+	}
+
+	public void user()
+	{
+		userState.user(this);
+	}
+}
+
+//##########################################################################################################################################
+//Decorator
+interface Discount {
+    
+  }
+  
+  class DiscountImpl implements Discount {
+  
+      @Override
+      public String decorate() {
+          return "Applying Discount to your meal......";
+      }
+  }
+  
+  abstract class discDecorator implements Discount {
+      private Discount disc;
+  
+       //standard constructors
+      @Override
+      public String decorate() {
+          return disc.decorate();
+     }
+  }
+
+class BronzeDiscount implements Subscriptions{
+
+    public static void main(String args[])
+    {
+        SubscriptionState stateContext = new SubscriptionState();
+        stateContext.setState(new Bronze());
+		stateContext.user();
+
+        double  dis,amount,mealprice,s;
+
+        mealprice= 20;
+        dis=10;  // 10 mean 10%
+
+        System.out.println("the original price of the meal is €"+mealprice);
+        System.out.println("discount rate= %"+dis);
+
+        s=100-dis;
+
+        amount= (s*mealprice)/100;
+        System.out.println("amount after discount= €"+amount);
+
+        
+    }
+
+}
+
+
+
+class SilverDiscount implements Subscriptions{
+
+    public static void main(String args[])
+    {
+
+        SubscriptionState stateContext = new SubscriptionState();
+        stateContext.setState(new Silver());
+		stateContext.user();
+
+        double  dis,amount,mealprice,s;
+
+        mealprice= 20;
+        dis=20;  // 20 mean 20%
+
+        System.out.println("the original price of the meal is €"+mealprice);
+        System.out.println("discount rate= %"+dis);
+
+        s=100-dis;
+
+        amount= (s*mealprice)/100;
+        System.out.println("amount after discount= €"+amount);
+    }
+
+}
+
+
+class GoldDiscount {
+
+    public static void main(String args[])
+    {
+
+        SubscriptionState stateContext = new SubscriptionState();
+        stateContext.setState(new Gold());
+		stateContext.user();
+
+        double  dis,amount,mealprice,s;
+
+        mealprice= 20;
+        dis=30;  // 30 mean 30%
+
+        System.out.println("the original price of the meal is €"+mealprice);
+        System.out.println("discount rate= %"+dis);
+
+        s=100-dis;
+
+        amount= (s*mealprice)/100;
+        System.out.println("amount after discount= €"+amount);
+
+    }
+
+}
+
+
+//##########################################################################################################################################
+class Meal_Calculator
+{
+	public static void main(String[] args)
+	{
+		SubscriptionState stateContext = new SubscriptionState();
+		stateContext.user();
+		stateContext.user();
+		stateContext.setState(new Silver());
+		stateContext.user();
+		stateContext.user();
+		stateContext.setState(new Gold());
+		stateContext.user();
+		stateContext.user();	
+		
+		
+	}
+}
