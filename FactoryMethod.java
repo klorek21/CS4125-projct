6a66897160f281e89aec5f96d06e5f76b800b53a
Elton111 <58089190+Elton111@users.noreply.github.com>
2022-12-03 00:16:59 +0000

    Add files via upload

--- FactoryMethod.java
+++ FactoryMethod.java
@@ -1,18 +1,16 @@
 
-// Create an interface
 // Interface named Meal
-public interface Meal {
+interface Meal {
 
    // Method inside interface
    void createMeal();
 }
 
-// Create concrete classes implementing the same interface
 
-// Class 1 is implementing the Meat interface
-// Concrete Class
+// Class 1
+// Class 1 will be implementing the Meat interface
 // Meat.java
-public class Meat implements Meal {
+class Meat implements Meal {
 
    // Overriding the method
    @Override
@@ -26,10 +24,10 @@ public class Meat implements Meal {
 }
 
 
-// Class 2 is implementing the Vegetable interface
-// Concrete class
+// Class 2
+// Abstract class
 // Vegetables.java
-public class Vegetables implements Meal {
+class Vegetables implements Meal {
 
    // Overriding the method
    @Override
@@ -41,10 +39,10 @@ public class Vegetables implements Meal {
 }
 
 
-// Class 3 is implementing the Carbs interface
-// Concrete class
+// Class 3
+// Concrete class extending the abstract class
 // Carbs.java
-public class Carbs implements Meal {
+class Carbs implements Meal {
 
    // Overriding the method
    @Override
@@ -58,8 +56,7 @@ public class Carbs implements Meal {
 
 // Class 4
 // MealFactory.java
-// Create a Factory to generate object of concrete class based on given information
-public class MealFactory {
+class MealFactory {
 	
    // Use getMeal method to get object of type meal 
    public Meal getMeal(String mealType){
@@ -83,29 +80,28 @@ public class MealFactory {
 
 // Class 5
 // FactoryPatternDemo.java
-// Use the Factory to get object of concrete class by passing an information such as type
-public class FactoryPatternDemo {
+public class FactoryMethod {
 
    public static void main(String[] args) {
       MealFactory mealFactory = new MealFactory();
 
-      // Get an object of Carbs and call its createMeal method.
+      // Get an object of Carbs and call its draw (createMeal) method.
       Meal meal1 = mealFactory.getMeal("CARBS");
 
 	  // Call 1
-      // Call createMeal method of Carbs
+      // Call draw method of Carbs
       // 1st Meal
       meal1.createMeal();
 	  
-      // Get an object of Meat and call its createMeal method.
+      // Get an object of Meat and call its draw (createMeal) method.
       Meal meal2 = mealFactory.getMeal("MEAT");
 	
 	  // Call 2
-      // Call createMeal method of MEAT
+      // Call draw method of MEAT
       // 2nd Meal
       meal2.createMeal();
 
-      // Get an object of Vegetables and call its createMeal method.
+      // Get an object of Vegetables and call its draw (createMeal) method.
       Meal meal3 = mealFactory.getMeal("VEGETABLES");
 
 	  // Call 3
